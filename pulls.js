4a633499a178d2fa36e7e0331e73267edaef1ce2
Vasco Fernandes <vasco.box@gmail.com>
2012-08-09 18:15:16 +0200

    initial implementation

--- pulls.js
+++ pulls.js
@@ -1,40 +1,76 @@
-var restify = require('restify');
+var restify = require('restify'),
+	cradle  = require('cradle');
+
+var db = new(cradle.Connection)().database('branches');
+
+//configure server
 
 var server = restify.createServer({
   name: 'pulls',
 });
 
-/*
-function send(req, res, next) {
-   res.send({some: 24});
-   return next();
+server.use(restify.bodyParser());
+
+//helpers
+function exit_on_error(err, res){ if(err) console.log(err); process.exit(1); }
+
+
+//gets
+function branch(req, res, next){
+	var	user = req.params.user,
+		repo = req.params.repo,
+		name = req.params.branch;
+
+	var id = user+':'+repo+':'+name;
+
+	db.get(id, function(err, branch){
+		res.send(200, branch);
+		next();
+	}); 
 }
 
-server.post('/hello', function create(req, res, next) {
-	res.send(201, Math.random().toString(36).substr(3, 8));
-	return next();
-});
+server.get('/:user/:repo/:branch/', branch);
 
-server.put('/hello', send);
-server.get('/hello/:name', send);
-server.head('/hello/:name', send);
- 
-server.del('hello/:name', function rm(req, res, next) {
-	res.send(204);
-	return next();
-});
-*/
 
+//github hooks
+function pullrequest(req, res, next){
+	//receive hook for github
+	console.log("received a pullrequest update");
+	var pr = JSON.parse(req.body);
+
+
+	var	user = pr.pull_request.head.repo.owner.login,
+		repo = pr.pull_request.head.repo.name,
+		name = pr.pull_request.head.ref;
+
+	var id = user+':'+repo+':'+name; 
+	
+	db.get(id, function (err, branch) {
+	  if(err){
+	  	branch = {};
+	  }
+
+	  branch.status = pr.action;
+	  branch.commit = pr.pull_request.head.commit;
+	  branch.pullrequest = pr.pull_request;
+
+	  db.save(id, branch, exit_on_error);
+  	});
+
+
+	console.log(id);
+	res.send(200, id);
+	
+	return next();
+}
+
+function comment(req, res, next){
+	//receive hook for gihub
+}
 
 server.post('/github/pullrequest/', pullrequest);
 server.post('/github/comment/', comment);
 
-function pullrequest(req, res, next){
 
-}
-
-
-
-
-
+//start server
 server.listen(8080);
